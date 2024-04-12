5367827c5f51cce9a5b64c271fd32b0a622efb28
NoBadVibes79 <krazerog@outlook.com>
2024-04-12 18:28:59 +0300

    Welcome to world

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from flask import Flask, render_template
+import psycopg2
+import sys
+import logging
 
+
+app = Flask(__name__)
+
+pg = psycopg2.connect('''
+    host=localhost
+    port=5432
+    user=postgres
+    password=rootroot
+    dbname=postgres
+''')
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        stream=sys.stdout,
+        format="%(asctime)s - %(message)s", # - %(levelname)s - %(name)s -
+        datefmt="%d-%m-%Y %H:%M:%S"
+    )
+    app.run(host='0.0.0.0', debug=True)
